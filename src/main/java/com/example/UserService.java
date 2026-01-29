0373644a01ad9a368954be76d8c2456890cf3dfa
Nipun-Milinda <nipunmilinda@gmail.com>
2026-01-29 10:48:17 +0530

    fix quality issues

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -3,6 +3,7 @@ package main.java.com.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.sql.SQLException;
 
 public class UserService {
 
@@ -10,25 +11,25 @@ public class UserService {
     private String password = "admin123";
 
     // VULNERABILITY: SQL Injection
-    public void findUser(String username) throws Exception {
+    public void findUser(String username) throws SQLException {
 
-        Connection conn =
-            DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
+             Statement st = conn.createStatement()) {
 
-        Statement st = conn.createStatement();
+            String query =
+                "SELECT * FROM users WHERE name = '" + username + "'";
 
-        String query =
-            "SELECT * FROM users WHERE name = '" + username + "'";
-
-        st.executeQuery(query);
+            st.executeQuery(query);
+        }
     }
 
     // EVEN WORSE: another SQL injection
-    public void deleteUser(String username) throws Exception {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
-        Statement st = conn.createStatement();
-        String query = "DELETE FROM users WHERE name = '" + username + "'";
-        st.execute(query);
+    public void deleteUser(String username) throws SQLException {
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
+             Statement st = conn.createStatement()) {
+            String query = "DELETE FROM users WHERE name = '" + username + "'";
+            st.execute(query);
+        }
     }
 
     // SMELL: Unused method
