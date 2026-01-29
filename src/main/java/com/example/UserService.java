4d411e9f6f2404ebd36c7102f06cc75e3eaf7c5f
Nipun-Milinda <nipunmilinda@gmail.com>
2026-01-29 10:56:17 +0530

    fix quality issues

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -2,7 +2,7 @@ package main.java.com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UserService {
@@ -13,22 +13,22 @@ public class UserService {
     // VULNERABILITY: SQL Injection
     public void findUser(String username) throws SQLException {
 
+        String query = "SELECT * FROM users WHERE name = ?";
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
-             Statement st = conn.createStatement()) {
+             PreparedStatement st = conn.prepareStatement(query)) {
 
-            String query =
-                "SELECT * FROM users WHERE name = '" + username + "'";
-
-            st.executeQuery(query);
+            st.setString(1, username);
+            st.executeQuery();
         }
     }
 
     // EVEN WORSE: another SQL injection
     public void deleteUser(String username) throws SQLException {
+        String query = "DELETE FROM users WHERE name = ?";
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
-             Statement st = conn.createStatement()) {
-            String query = "DELETE FROM users WHERE name = '" + username + "'";
-            st.execute(query);
+             PreparedStatement st = conn.prepareStatement(query)) {
+            st.setString(1, username);
+            st.execute();
         }
     }
 
